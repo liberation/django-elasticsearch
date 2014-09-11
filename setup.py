0d8c65c202094be6c73edeea2add207aec04eedb
lauxley <tissotrobin@gmail.com>
2014-09-11 18:25:06 +0200

    change the package name.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django_elasticsearch",
-    version = "0.1",
-    description = "Simple wrapper around py-elasticsearch to index/search a django Model.",
-    author = "Robin Tissot",
-    url = "https://github.com/liberation/django_elasticsearch",
-    packages = find_packages(),
+    name="django-elasticsearch",
+    version="0.1",
+    description="Simple wrapper around py-elasticsearch to index/search a django Model.",
+    author="Robin Tissot",
+    url="https://github.com/liberation/django_elasticsearch",
+    packages=find_packages(),
 )
