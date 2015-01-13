2763f473fa6a367c4d5807dfcd55b1452997ae98
lauxley <tissotrobin@gmail.com>
2015-01-13 10:33:40 +0100

    Add pystache to requirements, and change version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-elasticsearch",
-    version="0.2",
+    version="0.3",
     description="Simple wrapper around py-elasticsearch to index/search a django Model.",
     author="Robin Tissot",
     url="https://github.com/liberation/django_elasticsearch",
